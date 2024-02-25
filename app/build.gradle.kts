cf087236deba5add57a7a5f7486c90e38512678d
RBAGAPOROJR <ronald.ghub@gmail.com>
2024-02-24 19:56:05 -0500

    Implemented Four Fragment and Binding
    - Home Fragment
    - Second Fragment
    - Third Fragment
    - Fourth Fragment
    - enable Binding @ build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,39 +3,45 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android {
-    namespace = "com.example.kotlin_api_project"
-    compileSdk = 34
+    android {
+        namespace = "com.example.kotlin_api_project"
+        compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.example.kotlin_api_project"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        defaultConfig {
+            applicationId = "com.example.kotlin_api_project"
+            minSdk = 28
+            targetSdk = 34
+            versionCode = 1
+            versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+
+        viewBinding {
+
+            enable = true
+
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
 
-dependencies {
+    dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/kotlin_api_project/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_api_project/MainActivity.kt
@@ -2,10 +2,79 @@ package com.example.kotlin_api_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.annotation.NonNull
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.kotlin_api_project.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+class MainActivity : AppCompatActivity( ) {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate( savedInstanceState: Bundle? ) {
+        super.onCreate( savedInstanceState )
+
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.root )
+
+        // Load HomeFragment initially
+        loadFragment( HomeFragment( ), true  )
+
+        // Access views using binding
+        val bottomNavigationView = binding.mainBottomNav
+        val frameLayout = binding.mainFrameLayout
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when ( menuItem.itemId ) {
+                R.id.bottomNavHome -> {
+                    // Handle menu home click
+                    loadFragment(  HomeFragment( ), false  )
+                    true
+                }
+                R.id.bottomNav1 -> {
+                    // Handle menu 1 click
+                    loadFragment(  SecondFragment( ), false  )
+                    true
+                }
+                R.id.bottomNav2 -> {
+                    // Handle menu 2 click
+                    loadFragment(  ThirdFragment( ), false  )
+                    true
+                }
+                R.id.bottomNav3 -> {
+                    // Handle menu 3 click
+                    loadFragment(  FourthFragment( ), false  )
+                    true
+                }
+                else -> true
+            }
+
+        }
+    }
+
+    private fun loadFragment( fragment: Fragment, isAppInitialized: Boolean ) {
+        
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction( )
+
+        // Conditionally handle fragment transaction
+        if ( isAppInitialized ) {
+            
+            fragmentTransaction.add( R.id.mainFrameLayout, fragment )
+            
+        } else {
+            
+            fragmentTransaction.replace( R.id.mainFrameLayout, fragment )
+            
+        }
+
+        // Add the transaction to the back stack
+        fragmentTransaction.addToBackStack( null )
+        fragmentTransaction.commit( )
     }
 }
