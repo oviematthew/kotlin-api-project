cbced5cde53f09c355f1e143b583bf7c4dc2078c
Imasha Ekanayake <emashaedev@gmail.com>
2024-02-26 14:52:53 -0500

    Event locator changes added

--- app/src/main/java/com/example/kotlin_api_project/network/ApiService.kt
+++ app/src/main/java/com/example/kotlin_api_project/network/ApiService.kt
@@ -1,6 +1,15 @@
 package com.example.kotlin_api_project.network
 
+import com.example.kotlin_api_project.model.Event
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
-
+    @GET("events")
+    suspend fun getNearbyEvents(
+        @Query("location") location: String,
+        @Query("latitude") latitude: Double?,
+        @Query("longitude") longitude: Double?
+    ): List<Event>
 }
--- app/src/main/java/com/example/kotlin_api_project/repository/ApiRepository.kt
+++ app/src/main/java/com/example/kotlin_api_project/repository/ApiRepository.kt
@@ -1,4 +1,12 @@
 package com.example.kotlin_api_project.repository
 
+import com.example.kotlin_api_project.model.Event
+import com.example.kotlin_api_project.network.ApiService
+
 class ApiRepository {
+
+    suspend fun getNearbyEvents(apiService: ApiService, location: String?, latitude: Double?, longitude: Double?): List<Event>? {
+        val response = apiService.getNearbyEvents(location ?: "", latitude, longitude)
+        return response
+    }
 }
